9e53f13deeaa225af727de89dffda4113c40d750
fatimaqais <fatima_qais@hotmail.com>
2023-07-05 22:09:29 +0000

    Change button, text when clicked and increment

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -7,15 +7,30 @@ class StatefulGreeting extends React.Component {
         this.state = {
             introduction: "Hello",
             buttonText: "Exit",
+            count: 0
         };
     }
 
     handleClick() {
         this.setState({
-            introduction: "Goodbye",
-            buttonText: 'Enter',
+            introduction: this.state.introduction ==="Hello"? "Goodbye" : "Hello!",
+            buttonText: this.state.buttonText === "Exit"? "Enter" : "Exit",
+        },()=> {
+            console.log('new state', this.state.introduction)
+            console.log('new state', this.state.buttonText)
         });
         console.log(this.state.introduction)
+        console.log(this.state.buttonText)
+    }
+
+    increment() {
+        this.setState((prevState, prevProps) => {
+            console.log("previous State:", prevState)
+            console.log("previous State:", prevState)
+            return {
+                count: prevState.count + 1
+            }
+        })
     }
 
     render () {
@@ -23,6 +38,8 @@ class StatefulGreeting extends React.Component {
             <div>
                 <h1>{this.state.introduction} {this.props.greeting}</h1>
                 <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.increment()}>Increment</button>
+                <p>You've clicked {this.state.count} times</p>
             </div>
         )
     }
